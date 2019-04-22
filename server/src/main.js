f56f50910be5dfc2cd2fc12fe3267d92b93fb3cb
lukakralj <luka.kralj2@gmail.com>
2019-04-22 11:56:37 +0100

    Add a simple html file to test socket.io endpoints with

--- server/src/main.js
+++ server/src/main.js
@@ -1,16 +1,24 @@
 /**
+ * This module provides the socket.io endpoint for remote access.
  * 
  * @module 
  * @author Luka Kralj
  * @version 1.0
  */
 
-
 const app = require('express')();
 const server = require('http').Server(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(server);
 const logger = require('./util/logger');
 const config = require('../config/config.json');
+
+logger.info("Server has started.");
+
+logger.info("Starting authenticator...")
+const authenticator = require('./util/authenticator');
+logger.info("Authenticator has started.")
+
+logger.info("Server's public key is:" + authenticator.getServerPublicKey());
 
 server.listen(config.port, () => {
     logger.info("Listening on port: " + config.port + "...");
@@ -18,9 +26,14 @@ server.listen(config.port, () => {
 
 io.on('connection', (socket) => {
     logger.info(`Socket ${socket.id} connected`);
-    socket.emit("connected");
+    //socket.emit("connected");
 
     socket.on("disconnect", () => {
         logger.info(`Socket ${socket.id} disconnected`);
     });
+
+    socket.on("msg", (msg) => {
+        console.log("Received: " + msg);
+        socket.emit("res", "all good");
+    })
 });
